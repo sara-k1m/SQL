483856ebac449bb6280ca76cde8bc829f24c8c51
Sara Kim <74256435+sara-k1m@users.noreply.github.com>
2025-02-07 11:35:54 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT 
+       D.ID, 
+       D.EMAIL, 
+       D.FIRST_NAME, 
+       D.LAST_NAME
+FROM DEVELOPERS D
+JOIN SKILLCODES S
+    ON (D.SKILL_CODE & S.CODE) > 0
+    -- 비트 연산자 &로 CODE 값이 겹치는지 확인
+WHERE S.CATEGORY = 'Front End'
+ORDER BY D.ID;
